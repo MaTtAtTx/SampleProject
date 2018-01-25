69bc02b70091de6b7293c51876801489b834549c
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2018-01-25 09:53:27 -0700

    Added more arrays to project.

--- SampleProject/Controller/Controller.cpp
+++ SampleProject/Controller/Controller.cpp
@@ -28,9 +28,21 @@ void Controller :: start()
 void Controller :: createArray()
 {
     double balance[10] = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 11.0};
+    int ages[5] = {12, 15, 76, 16, 17};
+    string names[3] = {"Sally", "Tom", "Bill"};
     
     for (int index = 0; index < 10; index++)
     {
         cout << "The account balance is " << balance[index] * balance[index] << endl;
+    }
+    
+    for (int index = 0; index < 5; index ++)
+    {
+        cout << "I know people that are the age of " << ages[index];
+    }
+    
+    for (int index = 0; index < 3; index ++)
+    {
+        cout << "One of my friend's name is " << names[index];
     }
 }
